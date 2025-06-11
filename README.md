931b272448493e8f66b4fad541eb58ad8d911a28
Cleminhoo <clement-thomas.berthaud@sigma-clermont.fr>
2025-06-11 12:19:36 +0200

    add information

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gs_mini_feature_extraction
 hello
+
+## Rosrun 
+```
+ ros2 run vb_tactile_sensors gs_image_depth
+ ```
+ 
---
+++ gs_feature_extraction/optical_flow.py
@@ -0,0 +1,116 @@
+import numpy as np
+import cv2 as cv
+import argparse
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from std_msgs.msg import Float32MultiArray
+import math
+
+
+parser = argparse.ArgumentParser(description='This sample demonstrates Lucas-Kanade Optical Flow calculation. \
+                                              The example file can be downloaded from: \
+                                              https://www.bogotobogo.com/python/OpenCV_Python/images/mean_shift_tracking/slow_traffic_small.mp4')
+parser.add_argument('image', type=str, help='path to image file')
+args = parser.parse_args()
+cap = cv.VideoCapture(args.image)
+
+class ImageSubscriberPublisher(Node):
+    def __init__(self):
+        super().__init__('image_subscriber_publisher')
+
+        # Subscriber
+        self.subscription = self.create_subscription(
+            Image,
+            '/gs_mini_image',
+            self.image_callback,
+            10
+        )
+
+        # Publisher
+        self.publisher = self.create_publisher(
+            Image,
+            '/gs_opt_flow_img',
+            10
+        )
+
+        # CV Bridge
+        self.bridge = CvBridge()
+        self.get_logger().info('Node initialized: Subscribed to /gs_mini_img and publishing to /gs_opt_flow_img')
+
+
+     def image_callback(self, msg):
+        try:
+            self.get_logger().info(f"Image encoding: {msg.encoding}")
+
+            # Décodage de l'image ROS
+            if msg.encoding == 'bgr8':
+                cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            else:
+                cv_image = self.bridge.imgmsg_to_cv2(msg)
+
+    
+
+
+# params for ShiTomasi corner detection
+
+feature_params = dict( maxCorners = 100,
+                       qualityLevel = 0.3,
+                       minDistance = 7,
+                       blockSize = 7 )
+# Parameters for lucas kanade optical flow
+
+lk_params = dict( winSize  = (15, 15),
+                  maxLevel = 2,
+                  criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+
+# Create some random colors
+
+color = np.random.randint(0, 255, (100, 3))
+
+# Take first frame and find corners in it
+
+ret, old_frame = cap.read()
+old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
+p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+
+# Create a mask image for drawing purposes
+
+mask = np.zeros_like(old_frame)
+while(1):
+    ret, frame = cap.read()
+    if not ret:
+        print('No frames grabbed!')
+        break
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    
+    # calculate optical flow
+    
+    p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    
+    # Select good points
+    
+    if p1 is not None:
+        good_new = p1[st==1]
+        good_old = p0[st==1]
+    
+    # draw the tracks
+    
+    for i, (new, old) in enumerate(zip(good_new, good_old)):
+        a, b = new.ravel()
+        c, d = old.ravel()
+        mask = cv.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
+        frame = cv.circle(frame, (int(a), int(b)), 5, color[i].tolist(), -1)
+    img = cv.add(frame, mask)
+    cv.imshow('frame', img)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+   
+    # Now update the previous frame and previous points
+    
+    old_gray = frame_gray.copy()
+    p0 = good_new.reshape(-1, 1, 2)
+
+cv.destroyAllWindows()
