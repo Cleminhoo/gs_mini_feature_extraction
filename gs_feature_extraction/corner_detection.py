f57d1e289301c24e3d52e1d0576a41565ab92e86
Cleminhoo <clement-thomas.berthaud@sigma-clermont.fr>
2025-06-17 10:14:13 +0200

    approximation à une ligne

--- gs_feature_extraction/corner_detection.py
+++ gs_feature_extraction/corner_detection.py
@@ -48,6 +48,7 @@ class CornerDetectionNode(Node):
             frame = cv_image.copy()
 
         #frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        frame_bn = cv_image.copy()*0
 
         # Détection des coins
         corners = cv.goodFeaturesToTrack(
@@ -62,18 +63,36 @@ class CornerDetectionNode(Node):
 
         # Affichage des coins détectés
         if corners is not None:
+            points = []
             for i in range(corners.shape[0]):
                 x, y = corners[i, 0]
-                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-                cv.circle(frame, (int(x), int(y)), 4, color, -1)
+                points.append((x,y))
+                color = (255,255, 255)
+                cv.circle(frame_bn, (int(x), int(y)), 1, color, -1)
+
+            # Convertir en array numpy
+            points = np.array(points)
+            x_vals = points[:, 0]
+            y_vals = points[:, 1]
+
+            # Ajustement par une droite : y = m*x + b
+            if len(x_vals) >= 2:  # au moins 2 points requis
+                m, b = np.polyfit(x_vals, y_vals, 1)
+
+                # Calculer 2 points pour tracer la droite
+                x0, x1 = int(np.min(x_vals)), int(np.max(x_vals))
+                y0, y1 = int(m * x0 + b), int(m * x1 + b)
+
+                # Tracer la droite sur l’image
+                cv.line(frame_bn, (x0, y0), (x1, y1), (255, 0, 0), 1)
 
         # Affichage pour debug
-        cv.imshow("Corners", frame)
+        cv.imshow("Corners", frame_bn)
         cv.waitKey(1)
 
         # Publication ROS
         try:
-            msg_out = self.bridge.cv2_to_imgmsg(frame, encoding='passthrough')
+            msg_out = self.bridge.cv2_to_imgmsg(frame_bn, encoding='passthrough')
             self.publisher.publish(msg_out)
         except Exception as e:
             self.get_logger().error(f"CV Bridge publish error: {e}")
